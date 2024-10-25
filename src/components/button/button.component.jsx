cf2f1201f1cb5e3155f80287c73d00f31523adf4
bharath konatham <bharathreddyk121@gmail.com>
2024-10-24 23:24:29 -0400

    completed stripe payment local netlify not working

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,17 +1,17 @@
 
 import './button.styles.scss'
-
+import Spinner from '../spinner/spinner.component'
 const BUTTON_TYPE_CLASSES = {
     google: 'google-sign-in',
     inverted: 'inverted'
 }
 
-const Button = ({children,buttonType,...otherProps})=>{
+const Button = ({children,buttonType,isProcessing,...otherProps})=>{
 
     return(
         <button className= {` button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} 
         {...otherProps} >
-            {children}
+          {isProcessing? <Spinner/>: children}
         </button>
     )
 }
